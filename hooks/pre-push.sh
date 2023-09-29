77221d44a88522b0e5db877a74f21f23c0127ace
Ram Basnet <rambasnet@gmail.com>
2023-09-29 16:21:06 -0600

    update hooks

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Running pre-commit hook"
+echo "Running pre-push hook"
 ./hooks/run-test.sh
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
- echo "Tests must pass before commit!"
+ echo "Tests must pass before push!"
  exit 1
 fi
