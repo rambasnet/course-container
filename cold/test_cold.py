edbebac3218a5cab97a3cd2743a7de8d43fcd115
Ram Basnet <rambasnet@gmail.com>
2023-09-30 15:42:01 -0600

    add test cold

--- cold/test_cold.py
+++ cold/test_cold.py
@@ -3,5 +3,8 @@ import cold
 def test_answer():
     assert cold.answer('-10') == 1
 
-def test_answer_fail():
+def test1_answer():
     assert cold.answer('-10') == 1
+
+def test2_answer():
+    assert cold.answer('-10 0 -30') == 1
