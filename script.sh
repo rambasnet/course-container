11cc4bdb3c7c5f8c4c6bc81848f16e6cc12954a7
Ram Basnet <rambasnet@gmail.com>
2023-10-14 01:07:41 +0000

    updated script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,8 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Working directory is $SCRIPT_DIR"
 
-chown user:users /home/user/.zsh_history
-chown user:users /home/user/.gitconfig
-# chown user:users --recursive /home/user
+#chown user:users /home/user/.zsh_history
+#chown user:users /home/user/.gitconfig
+chown user:users --recursive /home/user/. &> /dev/null
 PATH='${SCRIPT_DIR}:$PATH'
 export PATH
