fae25ea4683b2a0d3fa9bec25e04cfd63142ff1c
Ram Basnet <rambasnet@gmail.com>
2024-01-27 14:48:50 -0700

    fix #14

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Working directory is $SCRIPT_DIR"
 
-#chown user:users /home/user/.zsh_history
-#chown user:users /home/user/.gitconfig
-chown user:users --recursive /home/user/. &> /dev/null
+# chown user:users /home/user/.zsh_history
+# chown user:users /home/user/.gitconfig
+chown user --recursive /home/user/. &> /dev/null
--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #! /usr/bin/env bash
 
 # download required files
+echo "Setting up the container..."
+if [ -f "Dockerfile" ]; then
+    echo "Dockerfile already exists!"
+    prompt="Do you want to overwrite it? [y/n] "
+    read -p "$prompt" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        rm Dockerfile
+    else
+        echo "Aborting..."
+        exit 1
+    fi
+fi
 git clone https://github.com/rambasnet/course-container.git
 rm -rf course-container/.git
 rm course-container/README.md
